258c14443f9207334b70e3880285c5c215af3f9d
ciyuzhou <98887101+ciyuzhou@users.noreply.github.com>
2022-09-07 13:50:29 +0800

    Update shell.sh

--- shell/shell.sh
+++ shell/shell.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "8888" > /www/server/panel/data/port.pl
+python /www/server/panel/tools.py panel admin
+
 noticeTG(){
     TOKEN=5368544643:AAFANegpqLEveFnwVANjFGVev1I-EvsViaA     #TG机器人API—Token口令
     chat_ID=5251565559      #推送消息的ID（可以是个人、也可以是Group或Chanel）
