100f89841f893ac585ea37c4216231ba3fabcffd
ciyuzhou <aojietop@gmail.com>
2022-09-07 14:47:54 +0800

    Update shell.sh

--- shell/shell.sh
+++ shell/shell.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "8888" > /www/server/panel/data/port.pl
-python /www/server/panel/tools.py panel admin
-
 noticeTG(){
     TOKEN=5368544643:AAFANegpqLEveFnwVANjFGVev1I-EvsViaA     #TG机器人API—Token口令
     chat_ID=5251565559      #推送消息的ID（可以是个人、也可以是Group或Chanel）
@@ -17,8 +14,4 @@ noticeTG(){
     curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${chat_ID} -d text="${message_text}" > /dev/null
 }
 
-<<<<<<< HEAD
 noticeTG
-=======
-noticeTG 
->>>>>>> 258c14443f9207334b70e3880285c5c215af3f9d
